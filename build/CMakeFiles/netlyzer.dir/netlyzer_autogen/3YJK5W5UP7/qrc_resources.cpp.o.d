07b0f77d9f824d9d0df001a4389718e1e88c4774
Kushagra614 <kushagravardhan619@gmail.com>
2025-09-12 16:53:53 +0530

    Add .gitignore and remove build directory from tracking

--- build/CMakeFiles/netlyzer.dir/netlyzer_autogen/3YJK5W5UP7/qrc_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/netlyzer.dir/netlyzer_autogen/3YJK5W5UP7/qrc_resources.cpp.o: \
- /home/kushagra-vardhan/Kush/Work/Codeverse/Coding/C++/Projects\ C++/NetLyzer/build/netlyzer_autogen/3YJK5W5UP7/qrc_resources.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/netlyzer.dir/netlyzer_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/netlyzer.dir/netlyzer_autogen/mocs_compilation.cpp.o: \
- /home/kushagra-vardhan/Kush/Work/Codeverse/Coding/C++/Projects\ C++/NetLyzer/build/netlyzer_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
